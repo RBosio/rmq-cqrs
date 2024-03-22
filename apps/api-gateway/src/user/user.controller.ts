293aba3c4731a0837ca4e4fc0785071cabef9e4a
Fiido <roccobosio@gmail.com>
2024-03-22 03:41:10 -0300

    update user

--- apps/api-gateway/src/user/user.controller.ts
+++ apps/api-gateway/src/user/user.controller.ts
@@ -57,7 +57,7 @@ export class UserController {
         { cmd: 'updateUser' },
         {
           userId,
-          ...updateUserDto,
+          updateUserDto,
         },
       )
       .pipe(
--- apps/users/src/users.controller.ts
+++ apps/users/src/users.controller.ts
@@ -1,4 +1,4 @@
-import { CreateUserDto } from '@app/common';
+import { CreateUserDto, UpdateUserDto } from '@app/common';
 import { Controller, Get } from '@nestjs/common';
 import { CommandBus } from '@nestjs/cqrs';
 import {
@@ -8,6 +8,7 @@ import {
   RmqContext,
 } from '@nestjs/microservices';
 import { CreateUserCommand } from './commands/impl/create-user.command';
+import { UpdateUserCommand } from './commands/impl/update-user.command';
 
 @Controller()
 export class UsersController {
@@ -24,6 +25,23 @@ export class UsersController {
     channel.ack(message);
 
     await this.commandBus.execute(new CreateUserCommand(createUserDto));
+
+    return true;
+  }
+
+  @MessagePattern({ cmd: 'updateUser' })
+  async updateUser(
+    @Ctx() context: RmqContext,
+    @Payload() data: { userId: string; updateUserDto: UpdateUserDto },
+  ) {
+    const channel = context.getChannelRef();
+    const message = context.getMessage();
+
+    channel.ack(message);
+
+    await this.commandBus.execute(
+      new UpdateUserCommand(data.userId, data.updateUserDto),
+    );
 
     return true;
   }
