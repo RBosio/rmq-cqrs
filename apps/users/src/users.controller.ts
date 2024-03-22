ec045579cf2edfc892de9e6fe1feb96207093add
Fiido <roccobosio@gmail.com>
2024-03-22 03:22:24 -0300

    create user command

--- apps/users/src/users.controller.ts
+++ apps/users/src/users.controller.ts
@@ -1,4 +1,30 @@
+import { CreateUserDto } from '@app/common';
 import { Controller, Get } from '@nestjs/common';
+import { CommandBus } from '@nestjs/cqrs';
+import {
+  Ctx,
+  MessagePattern,
+  Payload,
+  RmqContext,
+} from '@nestjs/microservices';
+import { CreateUserCommand } from './commands/impl/create-user.command';
 
 @Controller()
-export class UsersController {}
+export class UsersController {
+  constructor(private commandBus: CommandBus) {}
+
+  @MessagePattern({ cmd: 'createUser' })
+  async createUser(
+    @Ctx() context: RmqContext,
+    @Payload() createUserDto: CreateUserDto,
+  ) {
+    const channel = context.getChannelRef();
+    const message = context.getMessage();
+
+    channel.ack(message);
+
+    await this.commandBus.execute(new CreateUserCommand(createUserDto));
+
+    return true;
+  }
+}
--- apps/users/src/users.module.ts
+++ apps/users/src/users.module.ts
@@ -1,18 +1,30 @@
 import { Module } from '@nestjs/common';
 import { RmqModule } from '@app/common/rmq/rmq.module';
 import { ConfigModule } from '@nestjs/config';
-import { DatabaseModule } from '@app/common';
+import { DatabaseModule, UserEntity, UserRepository } from '@app/common';
 import { UsersController } from './users.controller';
+import { CqrsModule } from '@nestjs/cqrs';
+import { CommandHandlers } from './commands/handlers';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserFactory } from './user.factory';
+import { UserSchemaFactory } from '@app/common/database/entities/user-schema.factory';
 
 @Module({
   imports: [
+    CqrsModule,
     ConfigModule.forRoot({
       isGlobal: true,
     }),
     RmqModule,
     DatabaseModule,
+    TypeOrmModule.forFeature([UserEntity]),
   ],
   controllers: [UsersController],
-  providers: [],
+  providers: [
+    ...CommandHandlers,
+    UserRepository,
+    UserFactory,
+    UserSchemaFactory,
+  ],
 })
 export class UsersModule {}
